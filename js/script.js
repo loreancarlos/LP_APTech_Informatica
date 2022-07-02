2b2d0cc84bd2c8775881047921a14ff3f86a093c
Lorean Carlos <lorean.carlos@gmail.com>
2022-07-02 12:53:25 -0300

    Updated in Work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 const checkBoxWrapper = document.querySelectorAll(".checkbox-wrapper");
+const inputCheckBox = document.querySelectorAll(".checkbox-wrapper input");
+const buttonWhatsApp = document.querySelector("footer .whatsapp-wrapper a img");
 
 checkBoxWrapper.forEach((element) => {
    element.addEventListener("click", () => {
-      console.log(element.children[0]);
-      element.children[0].toggleAttribute("checked");
+      element.firstElementChild.toggleAttribute("checked");
    });
 });
+
+
+buttonWhatsApp.addEventListener("mouseover", () => {
+   buttonWhatsApp.classList.add("shake-on");
+   buttonWhatsApp.classList.remove("shake-off");
+});
+
+buttonWhatsApp.addEventListener("mouseout", () => {
+   buttonWhatsApp.classList.remove("shake-on");
+   buttonWhatsApp.classList.add("shake-off");
+});
+
