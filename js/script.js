cb9ac5d29ef0e71417200488add485962c98d314
Lorean Carlos <lorean.carlos@gmail.com>
2022-07-21 17:24:35 -0300

    Updated in Work

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ const checkBoxWrapper = document.querySelectorAll(".checkbox-wrapper");
 const inputCheckBox = document.querySelectorAll(".checkbox-wrapper input");
 const buttonWhatsApp = document.querySelector("footer .whatsapp-wrapper a img");
 
+const submitButton = document.querySelector("#submitForm");
+const resendButton = document.querySelector("main>input");
+const widthSubmitButton = submitButton.getBoundingClientRect().width.toFixed(0);
+const widthRem = widthSubmitButton.toString().slice(0, -1);
+resendButton.setAttribute("style", `max-width:${widthRem}rem`);
+resendButton.addEventListener("click", () => {
+   window.scrollTo(0, 0);
+});
+
 checkBoxWrapper.forEach((element) => {
    element.addEventListener("click", () => {
       element.firstElementChild.toggleAttribute("checked");
