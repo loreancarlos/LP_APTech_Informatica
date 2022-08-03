96743b412dc484c5bcafcfb23b937ed8447b88c7
Lorean Carlos <lorean.carlos@gmail.com>
2022-08-03 10:14:24 -0300

    Test

--- js/controls.js
+++ js/controls.js
@@ -2,17 +2,18 @@ import { appMail } from "./appMail.js";
 
 export function Controls() {
 
-   function sendEmail() {
+   async function sendEmail() {
       const name = document.querySelector("#name").value;
       const email = document.querySelector("#email").value;
       const inputCheckBox = document.querySelectorAll(".checkbox-wrapper input");
 
       const tel = document.querySelector("#tel");
       const services = setServices(inputCheckBox);
+      const ip = await getIpClient();
 
-      const response = appMail.send({ name, email, tel, services });
-
-      console.log(response + " FIM");
+      /*     const response = appMail.send({ name, email, tel, services });
+    
+          console.log(response + " FIM"); */
    }
 
    function setServices(services) {
@@ -29,6 +30,11 @@ export function Controls() {
          return array = ["Não preencheu"];
       }
    }
+
+   function getIpClient() {
+         return fetch('https://api.ipify.org?format=json').then(data => data.json()).then(data => {console.log(data);console.log("AQUIII")});
+   }
+
 
    return { sendEmail }
 }
